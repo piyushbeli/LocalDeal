178a0a7b090afea88c41773e0f0fdd840817cf53
piyushbeli <piyush.beli@metacube.com>
2015-05-31 14:48:44 +0530

    changing bower installation path

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module RoR
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = true
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
 
     #Uncomment this line after setting up memcache for production and testing environment
     #config.cache_store = :dalli_store, "cache-1.example.com", "cache-2.example.com",
