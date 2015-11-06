d0efacb1287d65c96c703c450a3ab7b18827c500
piyushbeli <piyush.beli@metacube.com>
2015-11-07 01:20:11 +0530

    No need to handle the invalid credentials in interceptor.

--- app/assets/javascripts/www_common/services/httpInterceptor.js
+++ app/assets/javascripts/www_common/services/httpInterceptor.js
@@ -1,19 +1,10 @@
 appCommon.factory('AuthHttpResponseInterceptor',['$q','$window', function($q, $window){
     return {
-        response: function(response){
-            if (response.status === 401) {
+        /*responseError: function(rejection) {
+            /!*if (rejection.status === 401) {
                 alert("Invalid user, your session may have been expired. Please login again");
-                $window.location.reload();
             }
-            return response || $q.when(response);
-        },
-        responseError: function(rejection) {
-            if (rejection.status === 401) {
-                alert("Invalid user, your session may have been expired. Please login again");
-                $window.location.reload();
-            } else {
-                return $q.reject(rejection);
-            }
-        }
+            return $q.resolve(rejection);*!/
+        }*/
     };
 }])
