bb970bdef9f2c7d405e37498865597ced34e1a54
piyushbeli <piyush.beli@metacube.com>
2016-02-03 00:01:15 +0530

    Correcting the place for slug index in user and vendor table [2]

--- db/migrate/20150513101918_add_extra_fields_to_vendor.rb
+++ db/migrate/20150513101918_add_extra_fields_to_vendor.rb
@@ -4,7 +4,7 @@ class AddExtraFieldsToVendor < ActiveRecord::Migration
     add_column :vendors, :website, :string
     add_column :vendors, :is_verified, :boolean, null: false, default: false
     add_column :vendors, :about_me, :string
-    add_column :vendors, :slug, :string
+    add_column :vendors, :slug, :string, index: true, unique: true
     add_reference :vendors, :category, index: true
     add_column :vendors, :fb_page, :string
     add_column :vendors, :google_plus_page, :string
@@ -12,6 +12,4 @@ class AddExtraFieldsToVendor < ActiveRecord::Migration
     add_column :vendors, :instagram_page, :string
     add_foreign_key :vendors, :categories
   end
-
-  add_index :vendors, [:slug],     :unique => true
 end
